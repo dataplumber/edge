2af377e072124beae6b3af2ffc28db138a00849d
Adam P Stallard <stallard@linprog3.cs.fsu.edu>
2016-07-13 20:05:11 -0400

    Broke some again with last commit. Testing.

--- src/main/python/libraries/edge/httputility.py
+++ src/main/python/libraries/edge/httputility.py
@@ -1,13 +1,14 @@
 import tornado.httpclient
 
 class HttpUtility(object):
-    def getResponse(self, url, callback, body=None, json=False):
-        headers = {'Connection': 'close'}
-        if json:
-            headers['Content-Type'] = 'application/json; charset=UTF-8'
+    def getResponse(self, url, callback, body=None, json_body=False):
+        req_headers = {'Connection': 'close'}
+        if json_body:
+            req_headers['Content-Type'] = 'application/json; charset=UTF-8'
         if body is not None:
-            request = tornado.httpclient.HTTPRequest(url, method='POST', headers=headers, request_timeout=30, body=body)
+            print req_headers
+            request = tornado.httpclient.HTTPRequest(url, method='POST', headers=req_headers, request_timeout=30, body=body)
         else:
-            request = tornado.httpclient.HTTPRequest(url, method='GET', headers=headers, request_timeout=30)
+            request = tornado.httpclient.HTTPRequest(url, method='GET', headers=req_headers, request_timeout=30)
         httpClient = tornado.httpclient.AsyncHTTPClient()
         httpClient.fetch(request,callback=callback)
--- src/main/python/libraries/edge/writer/neo4jtemplateresponsewriter.py
+++ src/main/python/libraries/edge/writer/neo4jtemplateresponsewriter.py
@@ -106,7 +106,7 @@ class Neo4jTemplateResponseWriter(TemplateResponseWriter):
         url = self._configuration.get('neo4j', 'datasetUrl')
 
         httpUtility = HttpUtility()
-        httpUtility.getResponse(url+'/cypher', self._onNeo4jResponse, body=query, json=True)
+        httpUtility.getResponse(url+'/cypher', self._onNeo4jResponse, body=query, json_body=True)
 
     def _generateOpenSearchResponse(self, neo4jResponse, searchText, searchUrl, searchParams, pretty):
         pass
