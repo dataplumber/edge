c4ac25180479af7a5fc34d329f13d2a429c50964
Nga Quach <Nga.T.Chung@jpl.nasa.gov>
2017-04-04 13:04:05 -0700

    Added additional header to options request (oceanxtremes).

--- src/main/python/plugins/oceanxtremes/post/Writer.py
+++ src/main/python/plugins/oceanxtremes/post/Writer.py
@@ -17,6 +17,7 @@ class Writer(requestresponder.RequestResponder):
         self.requestHandler.set_header('Access-Control-Allow-Origin', '*')
         self.requestHandler.set_header('Allow', 'OPTIONS, GET, HEAD, POST')
         self.requestHandler.set_header('Access-Control-Allow-Headers', 'X-Requested-With')
+        self.requestHandler.set_header('Accept', 'application/json')
         self.requestHandler.set_status(200)
         self.requestHandler.finish()
 
